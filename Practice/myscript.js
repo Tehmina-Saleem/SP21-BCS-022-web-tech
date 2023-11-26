3adc700d0015eb39b31867e9e2151f3b2e4477be
Tehmina-Saleem <104255800+Tehmina-Saleem@users.noreply.github.com>
2023-11-26 10:25:18 +0500

    update

---
+++ Practice/myscript.js
@@ -0,0 +1,15 @@
+console.log("This is java scriot code")
+var a=89;
+var b= 29;
+console.log(a + b)
+function sum(a,b){
+   return a*b
+}
+result = sum(2, 4)
+console.log(result)
+function btnclicked(){
+    console.log('this button is clicked')
+}
+function update(){
+    console.log("updating result")
+}
